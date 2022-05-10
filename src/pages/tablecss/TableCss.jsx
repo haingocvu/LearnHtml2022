2e7a47ea7d5736a21820483216890ddc8882d224
Hai Vu Ngoc (BTS-DL) <hai.vungoc@vib.com.vn>
2022-05-10 15:55:23 +0700

    table

--- src/pages/tablecss/TableCss.jsx
+++ src/pages/tablecss/TableCss.jsx
@@ -1,7 +1,102 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  margin: auto;
+  margin-top: 5em;
+  tr {
+    :nth-child(even) {
+      background-color: #f2f2f2;
+    }
+    :hover {
+      background-color: coral;
+    }
+  }
+  th {
+    height: 3em;
+    background-color: #04aa6d;
+    color: white;
+  }
+  td {
+    text-align: center;
+    padding: 1em;
+  }
+`;
+
+const Wrapper = styled.div`
+  padding-bottom: 1em;
+  overflow-x: auto;
+`;
 
 function TableCss() {
-  return <div>TableCss</div>;
+  return (
+    <Wrapper>
+      <Table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Points</th>
+            <th>Points</th>
+            <th>Points</th>
+            <th>Points</th>
+            <th>Points</th>
+            <th>Points</th>
+            <th>Points</th>
+            <th>Points</th>
+            <th>Points</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>Eve</td>
+            <td>Jackson</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+          </tr>
+          <tr>
+            <td>Adam</td>
+            <td>Johnson</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Wrapper>
+  );
 }
 
 export default TableCss;
